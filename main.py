d28881fcd0277be0781e7151983c4ea52d5fe52c
Naymul Islam <naymul504@gmail.com>
2023-10-27 18:39:46 +0600

    show message when the image is saved into local computer and custom user functionality is added

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ type = type(text_input)
 print=type
 get_x = tk.Entry()
 get_y = tk.Entry()
+get_text_color = tk.Entry()
 original_image = None
 def open_file():
     file = filedialog.askopenfilename()
@@ -36,7 +37,7 @@ def open_file():
             image_width, image_height = original_image.size
             # text_x = (image_width - text_width) /2
             # text_y = (image_height - text_height) / 2
-            draw.text(xy=(int(get_x.get()),int(get_y.get())) ,text=text, fill="white", font=font)
+            draw.text(xy=(int(get_x.get()),int(get_y.get())) ,text=text, fill=get_text_color.get(), font=font)
 
             photoimage = ImageTk.PhotoImage(original_image)
             image_label.config(image=photoimage)
@@ -50,6 +51,9 @@ def save_file():
     file_path = filedialog.asksaveasfilename(defaultextension='.png', filetypes=[('PNG files', '*.png')])
     if file_path:
         original_image.save(file_path)
+        saved_message.config(text="Watermarked Image Saved Successfull")
+
+        
 ## Welcome Message
 welcome_message = tk.Label(text="Welcome To The Aquamark Pro", font=(25,15,'bold'))
 welcome_message.grid(row=0, column=1)
@@ -57,23 +61,28 @@ welcome_message.grid(row=0, column=1)
 enter_text = tk.Label(text="Enter Your Text Here")
 enter_x_loc = tk.Label(text="Enter The X value")
 enter_y_loc = tk.Label(text='Enter Y Value')
-
+enter_text_color = tk.Label(text="Enter The Color Of The Text")
+saved_message = tk.Label()
 ## Show the input label
 text_input.grid(row=1, column=1)
 get_x.grid(row=2,column=1)
 get_y.grid(row=3,column=1)
+get_text_color.grid(row=4,column=1)
 enter_text.grid(row=1, column=0)
 enter_x_loc.grid(row=2, column=0)
 enter_y_loc.grid(row=3, column=0)
+enter_text_color.grid(row=4, column=0)
 ## Show Preview Image
 image_label = tk.Label(window)
-image_label.grid(row=4, column=1)
+image_label.grid(row=5, column=1)
 ## Image uplaod button
 image_upload = tk.Button(text="Upload you image here", command=open_file)
-image_upload.grid(row=5, column=1)
+image_upload.grid(row=6, column=1)
 
-update_image = tk.Button(text="Save", command=save_file)
-update_image.grid(row=5, column=2)
+save_image = tk.Button(text="Save", command=save_file)
+save_image.grid(row=6, column=2)
+
+saved_message.grid(row=7,column=1)
 
 
 window.mainloop()
